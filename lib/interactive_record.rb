42ca9f3cc049b0ba94bcbcf66a45ccde22df0add
Vicki Hamm <vicki.cello@gmail.com>
2018-08-14 15:19:31 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,5 +21,11 @@ class InteractiveRecord
     column_names.compact
   end
 
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
 
 end
